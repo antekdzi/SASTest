77224e25b6e5556af44dd9822c58b996e33656b6
Antek <47010480+antekdzi@users.noreply.github.com>
2025-05-29 10:16:52 -0400

    Add files via upload

---
+++ Python_scikit_learn.py
@@ -0,0 +1,19 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+
+# Create sample data using NumPy
+X = np.array([[1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [6, 7]])
+y = np.array([0, 0, 0, 1, 1, 1])
+
+# Split data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Train a logistic regression model
+model = LogisticRegression()
+model.fit(X_train, y_train)
+
+# Make predictions on the test set
+predictions = model.predict(X_test)
+
+print("Predictions:", predictions)
